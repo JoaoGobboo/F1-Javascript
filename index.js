cbc5c9381e88f7fec07f3e4c3232b78baf0f60d1
João Victor Gobbo <146770743+JoaoGobboo@users.noreply.github.com>
2024-03-21 19:55:40 -0300

    javascript changes

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function placeBet() {
     }
 
     balance -= betAmount;
-
     document.getElementById("balance").innerText = balance;
 
     race(selectedDriver, betAmount);
@@ -29,51 +28,44 @@ function race(selectedDriver, betAmount) {
     const raceTrackWidth = document.getElementById("race-track").offsetWidth;
 
     const raceInterval = setInterval(function() {
-        let raceFinished = false;
-
-        cars.forEach(function(car) {
+        const newPositionX = Array.from(cars).map(car => {
             const currentPositionX = parseInt(car.style.transform.match(/-?\d+/g)[0]) || 0;
-            const currentPositionY = parseInt(car.style.transform.match(/-?\d+/g)[1]) || 0;
-            const newPositionX = currentPositionX + Math.floor(Math.random() * 10);
-
-            if (newPositionX >= raceTrackWidth) {
-                raceFinished = true;
-            }
-
-            if (raceFinished) {
-                clearInterval(raceInterval);
-
-                const winningDriver = Array.from(cars).indexOf(car) + 1;
-
-                if (winningDriver === selectedDriver) {
-                    balance += betAmount * 2;
-                    document.getElementById("result").innerText = "You won! Congratulations!";
-                } else {
-                    document.getElementById("result").innerText = "You lost! Better luck next time!";
-                }
-
-                document.getElementById("balance").innerText = balance;
-                raceInProgress = false;
-
-                resetCarPositions();
-            } else {
-                car.style.transform = "translate(" + newPositionX + "px, " + currentPositionY + "px)";
-            }
+            return currentPositionX + Math.floor(Math.random() * 10);
         });
+
+        if (newPositionX.some(x => x >= raceTrackWidth)) {
+            clearInterval(raceInterval);
+            handleRaceFinish(selectedDriver, betAmount, cars);
+        } else {
+            cars.forEach((car, index) => {
+                car.style.transform = `translate(${newPositionX[index]}px, ${car.offsetTop}px)`;
+            });
+        }
     }, 50);
+}
+
+function handleRaceFinish(selectedDriver, betAmount, cars) {
+    const winningDriver = Array.from(cars).findIndex(car => parseInt(car.style.transform.match(/-?\d+/g)[0]) >= raceTrackWidth) + 1;
+
+    const resultMessage = document.getElementById("result");
+    if (winningDriver === selectedDriver) {
+        balance += betAmount * 2;
+        resultMessage.innerText = "You won! Congratulations!";
+    } else {
+        resultMessage.innerText = "You lost! Better luck next time!";
+    }
+
+    document.getElementById("balance").innerText = balance;
+    raceInProgress = false;
+    resetCarPositions();
 }
 
 function resetCarPositions() {
     const cars = document.querySelectorAll(".car");
-    const initialPositionX = 20; // Posição X inicial
-    const offsetY = 20; // Diferença vertical entre os carros
 
-    cars.forEach(function(car, index) {
-        const newPositionY = index * offsetY; // Incrementando a posição Y para posicionar abaixo do carro anterior
-        car.style.transform = "translate(" + initialPositionX + "px, " + newPositionY + "px)"; // Definindo a posição transform
+    cars.forEach((car, index) => {
+        car.style.transform = `translate(20px, ${index * 20}px)`;
     });
 }
 
-window.onload = function() {
-    resetCarPositions();
-};
+window.onload = resetCarPositions;
